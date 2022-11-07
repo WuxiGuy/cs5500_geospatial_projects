60a55ed326a78900c6a29b9d09beca271fef74e8
peinanwang <105306727+peinanwang@users.noreply.github.com>
2022-11-07 15:54:47 -0800

    Delete templates directory

--- templates/js/app.js
+++
@@ -1,7 +0,0 @@
-const menu = document.querySelector('#mobile_menu');
-const menuLink = document.querySelector('.navbar_menu');
-
-menu.addEventListener('click', function() {
-  menu.classList.toggle('is-active');
-  menuLink.classList.toggle('active');
-})
